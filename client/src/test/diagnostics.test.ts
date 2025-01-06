8322842adfa097048f982e96b7497ef4fac5b9a4
Jameson Knutson <jamesonknutson@me.com>
2025-01-05 16:56:57 -0800

    refactor and setup biome, run fixes

--- client/src/test/diagnostics.test.ts
+++ client/src/test/diagnostics.test.ts
@@ -3,12 +3,12 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import * as vscode from 'vscode';
-import * as assert from 'assert';
-import { getDocUri, activate } from './helper';
+import * as assert from 'assert'
+import * as vscode from 'vscode'
+import { activate, getDocUri } from './helper'
 
 suite('Should get diagnostics', () => {
-  const docUri = getDocUri('diagnostics.txt');
+  const docUri = getDocUri('diagnostics.txt')
 
   test('Diagnoses uppercase texts', async () => {
     await testDiagnostics(docUri, [
@@ -30,30 +30,27 @@ suite('Should get diagnostics', () => {
         severity: vscode.DiagnosticSeverity.Warning,
         source: 'ex',
       },
-    ]);
-  });
-});
+    ])
+  })
+})
 
 function toRange(sLine: number, sChar: number, eLine: number, eChar: number) {
-  const start = new vscode.Position(sLine, sChar);
-  const end = new vscode.Position(eLine, eChar);
-  return new vscode.Range(start, end);
+  const start = new vscode.Position(sLine, sChar)
+  const end = new vscode.Position(eLine, eChar)
+  return new vscode.Range(start, end)
 }
 
-async function testDiagnostics(
-  docUri: vscode.Uri,
-  expectedDiagnostics: vscode.Diagnostic[],
-) {
-  await activate(docUri);
+async function testDiagnostics(docUri: vscode.Uri, expectedDiagnostics: vscode.Diagnostic[]) {
+  await activate(docUri)
 
-  const actualDiagnostics = vscode.languages.getDiagnostics(docUri);
+  const actualDiagnostics = vscode.languages.getDiagnostics(docUri)
 
-  assert.equal(actualDiagnostics.length, expectedDiagnostics.length);
+  assert.equal(actualDiagnostics.length, expectedDiagnostics.length)
 
   expectedDiagnostics.forEach((expectedDiagnostic, i) => {
-    const actualDiagnostic = actualDiagnostics[i];
-    assert.equal(actualDiagnostic.message, expectedDiagnostic.message);
-    assert.deepEqual(actualDiagnostic.range, expectedDiagnostic.range);
-    assert.equal(actualDiagnostic.severity, expectedDiagnostic.severity);
-  });
+    const actualDiagnostic = actualDiagnostics[i]
+    assert.equal(actualDiagnostic.message, expectedDiagnostic.message)
+    assert.deepEqual(actualDiagnostic.range, expectedDiagnostic.range)
+    assert.equal(actualDiagnostic.severity, expectedDiagnostic.severity)
+  })
 }
